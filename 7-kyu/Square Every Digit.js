6d3b6da316328c7b6d02e1f2b82018b7eb5e7572
Max Shylenko <maxshylenko@gmail.com>
2022-08-04 10:35:27 +0200

    Square Every Digit solution

---
+++ 7-kyu/Square Every Digit.js
@@ -0,0 +1,10 @@
+'use strict'
+
+// https://www.codewars.com/kata/546e2562b03326a88e000020
+
+function squareDigits(num) {
+  return +num.toString()
+    .split('')
+    .map(n => n ** 2)
+    .join('');
+}
