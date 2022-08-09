76b92847e6bc69c30abd0eba5637c8e98cc280ec
Max Shylenko <maxshylenko@gmail.com>
2022-08-09 10:43:55 +0200

    Solution

---
+++ 7-kyu/Sum of two lowest positive integers.js
@@ -0,0 +1,9 @@
+'use strict'
+
+// https://www.codewars.com/kata/558fc85d8fd1938afb000014/train/javascript
+
+function sumTwoSmallestNumbers(numbers) {  
+  const sortedNumbers = [...numbers].sort((a, b) => a - b);
+
+  return sortedNumbers[0] + sortedNumbers[1];
+}
