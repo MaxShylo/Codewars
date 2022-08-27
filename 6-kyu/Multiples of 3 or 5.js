49b8112c03120fadbe9f886825f97f9078e88138
Max Shylenko <maxshylenko@gmail.com>
2022-08-28 00:10:47 +0200

    Solution

---
+++ 6-kyu/Multiples of 3 or 5.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// https://www.codewars.com/kata/514b92a657cdc65150000006/train/javascript
+
+function solution(number) {
+  let sum = 0;
+
+  if (number < 0) {
+    return 0;
+  }
+
+  for (let i = 1; i < number; i++) {
+    if((i % 3 === 0) || (i % 5 === 0)) {
+      sum += i;
+    }
+  }
+
+  return sum;
+}
