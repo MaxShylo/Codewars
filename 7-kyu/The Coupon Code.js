ac65ccab651d58c89994c93b1113eb19d6bf3e23
Maksym Shylenko <maxshylenko@gmail.com>
2022-09-15 13:12:40 +0200

    Solution

---
+++ 7-kyu/The Coupon Code.js
@@ -0,0 +1,7 @@
+'use strict'
+
+// https://www.codewars.com/kata/539de388a540db7fec000642
+
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+  return (enteredCode === correctCode) && (new Date(currentDate) <= new Date(expirationDate));
+}
